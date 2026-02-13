5b4bd7b37aa9862efc9918bc8c9a5150e7f81fcc
Arngrimur <arngrimur24@ru.is>
2026-02-13 10:57:06 +0000

    Initial commit

---
+++ FritosJS/fritos.js
@@ -0,0 +1,53 @@
+class FritosObject {
+    constructor(selector) {
+        this.elements = Array.from(document.querySelectorAll(selector));    
+    }
+
+    hide(){
+        return
+    }
+
+    parent(){
+        return
+    }
+
+    ancestor(){
+        return
+    }
+
+    animate(){
+        return
+    }
+
+    find(){
+        return
+    }
+
+    onEvent(){
+        return
+    }
+
+    remoteCall(){
+        return
+    }
+
+    validation(){
+        return
+    }
+
+    prune(){
+        return
+    }
+
+    raise(){
+        return
+    }
+
+    attr(){
+        return
+    }
+
+    val(){
+        return
+    }
+}
