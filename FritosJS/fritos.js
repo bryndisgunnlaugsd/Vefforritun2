585f4df04c3deb76ad07adc41219e434049d19b4
arnarmani11 <arnarmani03@gmail.com>
2026-02-13 12:26:27 +0000

    my stuff

--- FritosJS/fritos.js
+++ FritosJS/fritos.js
@@ -43,16 +43,35 @@ class FritosObject {
         return
     }
 
-    raise(){
-        return
+    raise(level){
+        for(let i = 0; i < this.elements.length; i++) {
+            const element = this.elements[i];
+            for(let j = 0; j < level; j++) {
+                const parent = element.parentNode;
+                const grandparent = parent.parentNode;
+                grandparent.insertBefore(element, parent);
+            }
+        }
+        return this;
     }
 
-    attr(){
-        return
+    attrs(attr_name, value){
+        for(let i = 0; i < this.elements.length; i++) {
+            this.elements[i].setAttribute(attr_name, value)
+        }
+        return this;
     }
 
-    val(){
-        return
+    val(value){
+        if (value === undefined) {
+            return this.elements[0].value
+        }else {
+            for (let i = 0; i < this.elements.length; i++) {
+                this.elements[i].value = value
+            }
+            return this;
+        }
+
     }
 }
 const fritos = (selector) => {
