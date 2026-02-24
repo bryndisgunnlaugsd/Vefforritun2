c8c48f300908c4f576918317ccdbdc8894f585ea
Arngrimur <arngrimur24@ru.is>
2026-02-24 10:55:26 +0000

    Start

---
+++ recipe-api/my-app/src/components/header.tsx
@@ -0,0 +1,9 @@
+import "./header.css"
+
+export function Header() {
+    return(
+        <div className="header">
+            <h1>Explore Recipes</h1>
+        </div>
+    );
+}
---
+++ recipe-api/my-app/src/pages/Recipedetail.tsx
@@ -0,0 +1,7 @@
+export function Recipedetail() {
+  return (
+    <div>
+      <h1>Recipe Detail</h1>
+    </div>
+  );
+}
